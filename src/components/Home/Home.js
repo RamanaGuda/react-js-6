5ab454d64c573130850575273e1eb7a17d8b053f
RamanaGuda <131001891+RamanaGuda@users.noreply.github.com>
2023-10-22 20:35:42 +0530

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -20,7 +20,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> VARSHITH MUPPINIDI </strong>
+                <strong className="main-name">GUDA VENKATA RAMANAIAH </strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
