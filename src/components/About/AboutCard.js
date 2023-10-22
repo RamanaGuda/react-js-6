880fd776cd9a9801b961506b63eefe91c175bbfc
RamanaGuda <131001891+RamanaGuda@users.noreply.github.com>
2023-10-22 20:34:27 +0530

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,7 +8,7 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Varshith Muppinidi</span>
+            Hi Everyone, I am <span className="purple">GUDA VENKATA RAMANAIAH</span>
             from <span className="purple"> Andhra Pradesh, India.</span>&nbsp;
             I am a junior at VIT-AP University studying Computer Science and engineering with specilaization in Artificial Intelligence and Machine learning
             <br />
@@ -20,7 +20,7 @@ function AboutCard() {
           </p>
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Playing Sports
+              <ImPointRight /> Playing Cricket
             </li>
             <li className="about-activity">
               <ImPointRight /> Coding
